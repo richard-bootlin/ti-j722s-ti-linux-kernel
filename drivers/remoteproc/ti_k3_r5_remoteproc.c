935d6718a93931297e395a77d431193c46ed7885
Richard Genoud (TI) <richard.genoud@bootlin.com>
2026-04-03 16:27:23 +0200

    PENDING: ti_k3_r5_remoteproc: enable suspend in ipc-only mode for j7200/j721s2/j784s4...
    On Jacinto platforms, all the cores are powered-off during suspend,
    event the attached rprocs.
    
    If those rprocs were loaded by U-Boot at boot time, it's not the case at
    resume because only U-Boot SPL is run.
    
    So, the have to be loaded by Linux.
    
    At resume, the R5F cores that were previously in IPC-only mode will be
    loaded and started by the kernel, and thus will be in remoteproc mode.
    
    Signed-off-by: Richard Genoud (TI) <richard.genoud@bootlin.com>

--- drivers/remoteproc/ti_k3_r5_remoteproc.c
+++ drivers/remoteproc/ti_k3_r5_remoteproc.c
@@ -1548,7 +1548,7 @@ static const struct k3_r5_soc_data am65_j721e_soc_data = {
 	.tcm_ecc_autoinit = false,
 	.single_cpu_mode = false,
 	.is_single_core = false,
-	.core_data = &r5_data,
+	.core_data = &r5_jacinto_data,
 };
 
 static const struct k3_r5_soc_data j7200_j721s2_soc_data = {
@@ -1556,7 +1556,7 @@ static const struct k3_r5_soc_data j7200_j721s2_soc_data = {
 	.tcm_ecc_autoinit = true,
 	.single_cpu_mode = false,
 	.is_single_core = false,
-	.core_data = &r5_data,
+	.core_data = &r5_jacinto_data,
 };
 
 static const struct k3_r5_soc_data am64_soc_data = {
